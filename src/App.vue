7d03d04af98537171e4393d4f82ce5c4dabb266c
Baramatsu <b1018210.fun@gmail.com>
2019-06-03 17:52:12 +0900

    :sparkles: Drawerを実装

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-side-icon> </v-toolbar-side-icon>
+    <v-navigation-drawer
+      permanent
+      clipped
+      :floating="false"
+      :mini-variant="miniVariant"
+      absolute
+      overflow
+      app
+    >
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>view_list</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title>商品一覧 </v-list-tile-title>
+      </v-list-tile>
+    </v-navigation-drawer>
+
+    <v-toolbar app clipped-left>
+      <v-toolbar-side-icon
+        @click.stop="miniVariant = !miniVariant"
+      ></v-toolbar-side-icon>
       <v-img
         :src="require('./assets/amahako_bar.png')"
         height="50"
         contain
       ></v-img>
     </v-toolbar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      //
-    };
-  },
-};
+@Component
+export default class App extends Vue {
+  public miniVariant: boolean = true;
+}
 </script>
