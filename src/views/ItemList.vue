1cb9d72189b7b94747a8f411b875965196160e73
KazumaOhashi <b1016095@fun.ac.jp>
2019-06-11 15:05:37 +0900

    :sparkles:商品一覧ページでカテゴリのステータスが見れる

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,58 +1,66 @@
 <template>
-  <v-container>
-    <v-card>
-      <v-card-title>
-        商品一覧
-        <v-spacer></v-spacer>
-        <v-text-field
-          v-model="search"
-          append-icon="search"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table :headers="headers" :items="sweets" :search="search">
-        <template v-slot:items="props">
-          <td>
-            <v-avatar size="36px">
-              <img :src="props.item.imagePath" />
-            </v-avatar>
-          </td>
-          <td>
-            <span class="caption">{{ props.item.name }}</span>
-          </td>
-          <td>
-            <span class="caption">{{
-              shops[props.item.shop_id - 1].name
-            }}</span>
-          </td>
-          <td class="text-xs-right">
-            <span class="caption">{{ props.item.price }}</span>
-          </td>
-          <td class="text-xs-right">
-            <span class="caption">{{
-              new Date(props.item.updated_at).toLocaleString()
-            }}</span>
-          </td>
-          <td class="text-xs-center">
-            <v-icon small class="mr-2" @click="showItem(props.item.id)"
-              >visibility</v-icon
+  <v-card>
+    <v-card-title>
+      商品一覧
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table :headers="headers" :items="sweets" :search="search">
+      <template v-slot:items="props">
+        <td>
+          <v-avatar size="36px">
+            <img :src="props.item.imagePath" />
+          </v-avatar>
+        </td>
+        <td>
+          <span class="caption">{{ props.item.name }}</span>
+        </td>
+        <td>
+          <span class="caption">{{ shops[props.item.shop_id - 1].name }}</span>
+        </td>
+        <td>
+          <span class="caption">{{ props.item.price }}</span>
+        </td>
+        <td>
+          <span class="caption">
+            {{ new Date(props.item.updated_at).toLocaleString() }}
+          </span>
+        </td>
+        <td class="text-xs-center">
+          <span v-show="props.item.small_category_id != null">
+            <v-icon color="success" @click="showItem(props.item)"
+              >check_circle</v-icon
             >
-            <v-icon small class="mr-2" @click="editItem(props.item.id)"
-              >edit</v-icon
-            >
-            <v-icon small @click="deleteItem(props.item.id)">delete</v-icon>
-          </td>
-        </template>
-        <template v-slot:no-results>
-          <v-alert :value="true" color="error" icon="warning"
-            >Your search for "{{ search }}" found no results.</v-alert
-          >
-        </template>
-      </v-data-table>
-    </v-card>
-  </v-container>
+          </span>
+          <span v-show="props.item.small_category_id == null" class="caption">{{
+            props.item.small_category_id
+          }}</span>
+        </td>
+        <td>
+          <v-btn icon dark color="primary">
+            <v-icon @click="showItem(props.item)">visibility</v-icon>
+          </v-btn>
+          <v-btn icon dark color="grey">
+            <v-icon @click="editItem(props.item)">edit</v-icon>
+          </v-btn>
+          <v-btn icon dark color="red">
+            <v-icon @click="deleteItem(props.item)">delete</v-icon>
+          </v-btn>
+        </td>
+      </template>
+      <template v-slot:no-results>
+        <v-alert :value="true" color="error" icon="warning"
+          >Your search for "{{ search }}" found no results.</v-alert
+        >
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -73,6 +81,12 @@ export default class ItemList extends Vue {
     { text: '販売店舗', sortable: false, value: 'shop_id', align: 'left' },
     { text: '商品価格', value: 'price' },
     { text: '最終更新日', value: 'updated_at' },
+    {
+      text: 'ステータス',
+      sortable: false,
+      value: 'small_category_id',
+      align: 'left',
+    },
     { text: 'アクション', sortable: false, align: 'left' },
   ];
 
