8a9f7df973f813c6f883216a1228638361e465a1
KazumaOhashi <b1016095@fun.ac.jp>
2019-07-29 19:28:53 +0900

    :construction:商品詳細ページの新規作成

---
+++ src/views/ItemDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fluid>
+    <v-layout wrap>
+      ItemDetailPage
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import SweetsModel from '@/models/SweetsModel';
+import ShopMode from '@/models/ShopModel';
+import {
+  SmallSweetsCategory,
+  LargeSweetsCategory,
+} from '@/models/SweetsCategoryModel';
+import { sweetsSampleResponse } from '@/demodatas/Sweets';
+import { smallCategorySampleResponse } from '@/demodatas/SmallCategory';
+import { largeCategoriesSampleResponse } from '@/demodatas/LargeCategory';
+
+@Component
+export default class ItemDetail extends Vue {}
+</script>
