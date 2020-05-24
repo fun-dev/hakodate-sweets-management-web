543695a6ee1abaf5f910cd27148e06cd90d31cca
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-05-24 18:07:22 +0900

    refactor: srcディレクトリ作成

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>main</main>
+    </div>
+  );
+};
+
+export default Home;
