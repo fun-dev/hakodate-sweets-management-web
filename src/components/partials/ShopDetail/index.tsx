75c74756f9a8c9416b1bef18bb0c7ac95385d781
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-08-23 16:21:16 +0900

    feat: 店舗詳細ページのレイアウト

--- src/components/partials/ShopDetail/index.tsx
+++ src/components/partials/ShopDetail/index.tsx
@@ -1,5 +1,22 @@
+import * as Layout from './Layout';
+import { ShopInformation } from './ShopInformation';
+import { ShopProducts } from './ShopProducts';
+import { ShopCoupons } from './ShopCoupons';
+
 export const ShopDetail: React.FC = () => (
-  <>
-    <div>商品詳細ページ</div>
-  </>
+  <Layout.Wrapper>
+    <Layout.LeftItem>
+      <ShopInformation />
+    </Layout.LeftItem>
+
+    <Layout.RightContainer>
+      <Layout.TopRightItem>
+        <ShopProducts />
+      </Layout.TopRightItem>
+
+      <Layout.BottomRightItem>
+        <ShopCoupons />
+      </Layout.BottomRightItem>
+    </Layout.RightContainer>
+  </Layout.Wrapper>
 );
