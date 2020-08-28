c7279280a7d7c45e2dc2351f47516df8c1eac3d1
yukito <yuki.todate094@gmail.com>
2020-08-28 14:44:17 +0900

    feat: shopIdを子コンポーネントに引き渡し

--- src/components/partials/ShopDetail/ShopInformation/ShopInformation.tsx
+++ src/components/partials/ShopDetail/ShopInformation/ShopInformation.tsx
@@ -1,7 +1,18 @@
-import { Paper, Box } from '@material-ui/core';
+import { Paper, Box, List, ListItem, ListItemText } from '@material-ui/core';
+import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import { ShopInformationColumn } from '../ShopInformationColumn';
+import { useShop } from 'src/lib/api/requests/useShop';
 
-export const ShopInformation: React.FC = () => (
-  <Box height="100%" clone>
-    <Paper>店舗情報</Paper>
-  </Box>
-);
+type Props = {
+  shopId: number;
+};
+
+export const ShopInformation: React.FC<Props> = ({ shopId }) => {
+  return (
+    <Box height="100%" clone>
+      <Paper>
+        <ShopInformationColumn shopId={shopId} />
+      </Paper>
+    </Box>
+  );
+};
