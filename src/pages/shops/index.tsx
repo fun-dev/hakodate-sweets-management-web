ee3078900e932bc0aefb4d053fba2eeb3ab51cf2
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-09-09 14:46:03 +0900

    feat: ログインページ以外の閲覧を制限

--- src/pages/shops/index.tsx
+++ src/pages/shops/index.tsx
@@ -1,10 +1,24 @@
-import { NextPage } from 'next';
+import { NextPage, GetServerSideProps } from 'next';
 import { Shops } from 'src/components/partials/Shops';
+import nookies from 'nookies';
+import { verifyIdToken } from 'src/lib/firebase/admin';
 
 const ShopsPage: NextPage = () => (
   <>
     <Shops />
   </>
 );
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = nookies.get(context);
+  const { verified } = await verifyIdToken(cookies.token);
+
+  if (!verified) {
+    context.res.writeHead(302, { Location: '/login' });
+    context.res.end();
+  }
+
+  return { props: {} };
+};
 
 export default ShopsPage;
