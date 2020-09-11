dfb82073b7b59a7aad706d9f2a2cd9f92974e55f
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-09-11 21:48:26 +0900

    feat: カテゴリの通常時のUI

--- src/components/ProductTable/Columns/CategoriesColumn/Categories.tsx
+++ src/components/ProductTable/Columns/CategoriesColumn/Categories.tsx
@@ -1,14 +1,16 @@
 import { Sweet } from 'src/lib/api/models/Sweet';
-import { Box } from '@material-ui/core';
+import { Box, Chip } from '@material-ui/core';
 
 type Props = {
   categories: Sweet['small_categories'];
 };
 
 export const Categories: React.FC<Props> = ({ categories }) => (
-  <Box>
+  <Box display="flex" flexWrap="wrap">
     {categories.map((category) => (
-      <Box>{category.name}</Box>
+      <Box key={category.id} margin={0.5} clone>
+        <Chip label={category.name} />
+      </Box>
     ))}
   </Box>
 );
