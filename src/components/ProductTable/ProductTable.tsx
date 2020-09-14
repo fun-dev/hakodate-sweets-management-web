f330d8e2f960d53b5eba88da209e2f2b3b5592e5
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-09-14 21:21:29 +0900

    fix: ページのデフォルトサイズを10件に変更

--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -48,6 +48,8 @@ export const ProductTable: React.FC = () => {
             maxBodyHeight: tableBodyHeight,
             minBodyHeight: tableBodyHeight,
             draggable: false,
+            pageSize: 10,
+            pageSizeOptions: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
           }}
           localization={localization}
           editable={{
