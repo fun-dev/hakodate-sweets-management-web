adcc719b6fb825fde8cc1bd602a758920c68698a
Yuta Takahashi <yuta.takahashi.dev@gmail.com>
2020-09-14 20:56:11 +0900

    feat: カテゴリ編集

--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -7,6 +7,8 @@ import { tableIcons } from './TableIcons';
 import { theme } from 'src/styles/theme';
 import { localization } from './Localization';
 import { AddBox } from '@material-ui/icons';
+import { updateSweetRequest } from 'src/lib/api/requests/updateSweetRequest';
+import { useSnackbar } from 'notistack';
 
 const appHeaderHeight = 64;
 const padding = theme.spacing(3);
@@ -15,7 +17,8 @@ const tableHeaderHeight = 121;
 const tableBodyHeight = `calc(100vh - ${appHeaderHeight}px - ${padding * 2}px - ${tableHeaderHeight}px)`;
 
 export const ProductTable: React.FC = () => {
-  const { data } = useSweets();
+  const { data, mutate } = useSweets();
+  const { enqueueSnackbar } = useSnackbar();
 
   return (
     <Box height={tableContainerHeight} clone>
@@ -43,12 +46,47 @@ export const ProductTable: React.FC = () => {
             },
             maxBodyHeight: tableBodyHeight,
             minBodyHeight: tableBodyHeight,
+            draggable: false,
           }}
           localization={localization}
           editable={{
             onRowUpdate: (newData, oldData) =>
-              new Promise((resolve, reject) => {
-                resolve();
+              new Promise(async (resolve, reject) => {
+                console.log('newData', newData);
+                console.log('oldData', oldData);
+                const result = await updateSweetRequest(
+                  { id: newData.id },
+                  { sweet: { small_category_ids: newData.small_categories.map((x) => x.id) } }
+                );
+                if (!result) {
+                  enqueueSnackbar('編集権限がありません。', {
+                    variant: 'error',
+                    anchorOrigin: {
+                      vertical: 'top',
+                      horizontal: 'center',
+                    },
+                  });
+                  reject();
+                } else if (result.status === 200) {
+                  mutate((data) => ({
+                    ...data,
+                    sweets: data.sweets.map((sweet) => {
+                      if (sweet.id === newData.id) {
+                        return newData;
+                      } else {
+                        return sweet;
+                      }
+                    }),
+                  }));
+                  enqueueSnackbar('更新されました。', {
+                    variant: 'success',
+                    anchorOrigin: {
+                      vertical: 'top',
+                      horizontal: 'center',
+                    },
+                  });
+                  resolve();
+                }
               }),
             editTooltip: (sweet) => `${sweet.name}を編集`,
           }}
